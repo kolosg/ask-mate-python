e93778781ac6734ee958ab147bd0527e21148849
Milán <matolcsi.milan@gmail.com>
2019-03-18 13:25:54 +0100

    Listing questions and answers with SQL

--- data_manager.py
+++ data_manager.py
@@ -1,95 +1,23 @@
-import connection
-from datetime import datetime
-import util
+import database_connection
 
-QUESTION_FIELDS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWER_FIELDS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+@database_connection.connection_handler
+def list_all_question(cursor):
+    cursor.execute("""
+                    SELECT * FROM Question
+                    ORDER BY submission_time DESC;
+                    """)
+    all_question = cursor.fetchall()
+    return all_question
 
-def sort_data_by_value(value, name):
-    list_of_questions = []
-    connection.read_data_from_csv('templates/'+ name + '.csv', list_of_questions)
-    for j in reversed(range(len(list_of_questions))):
-        for k in range(j):
-            if list_of_questions[k][value] < list_of_questions[k + 1][value]:
-                list_of_questions[k], list_of_questions[k + 1] = list_of_questions[k + 1], list_of_questions[k]
-    if name == 'question':
-        connection.write_data_to_csv('templates/' + name + '.csv', QUESTION_FIELDS, list_of_questions)
-    else:
-        connection.write_data_to_csv('templates/' + name + '.csv', ANSWER_FIELDS, list_of_questions)
-    return list_of_questions
-
-
-def convert_unix_time_to_date(name):
-    data_list = []
-    connection.read_data_from_csv('templates/'+ name +'.csv', data_list)
-    for data in data_list:
-        data['submission_time'] = int(data['submission_time'])
-        data['submission_time'] = datetime.utcfromtimestamp(data['submission_time']).strftime('%Y-%m-%d %H:%M:%S')
-
-    return data_list
-
-
-def add_question(title_or_question_id, message):
-    ID = util.generate_id("templates/question.csv")
-    DATE = util.add_submission_time()
-    connection.append_data_to_csv('templates/question.csv', QUESTION_FIELDS, ID, DATE, title_or_question_id, message)
-
-
-def add_answer(title_or_question_id, message):
-    ID = util.generate_id("templates/answer.csv")
-    DATE = util.add_submission_time()
-    connection.append_data_to_csv("templates/answer.csv", ANSWER_FIELDS, ID, DATE, title_or_question_id, message)
-
-
-def edit_question(title, message, quest_id):
-    questions = []
-    connection.read_data_from_csv("templates/question.csv", questions)
-    DATE = util.add_submission_time()
-    for question in questions:
-        if quest_id == question['id']:
-            question['title'] = title
-            question['message'] = message
-            question['submission_time'] = DATE
-    connection.write_data_to_csv("templates/question.csv", QUESTION_FIELDS, questions)
-
-
-def delete_question(quest_id):
-    question_data = []
-    answer_data = []
-    connection.read_data_from_csv("templates/question.csv", question_data)
-    connection.read_data_from_csv("templates/answer.csv", answer_data)
-    for question in question_data:
-        if quest_id == question['id']:
-            question_data.remove(question)
-    for answer in answer_data[::-1]:
-        if quest_id == answer['question_id']:
-            answer_data.remove(answer)
-    connection.write_data_to_csv("templates/question.csv", QUESTION_FIELDS, question_data)
-    connection.write_data_to_csv("templates/answer.csv", ANSWER_FIELDS, answer_data)
-
-
-def delete_answer(answer_id):
-    answer_data = []
-    connection.read_data_from_csv("templates/answer.csv", answer_data)
-    for answer in answer_data:
-        if answer_id == answer['id']:
-            question_id = answer['question_id']
-            answer_data.remove(answer)
-    connection.write_data_to_csv("templates/answer.csv", ANSWER_FIELDS, answer_data)
-    return question_id
+@database_connection.connection_handler
+def list_answers(cursor):
+    cursor.execute("""
+                    SELECT * FROM answer; 
+    """)
+    all_answer = cursor.fetchall()
+    return all_answer
 
 
 def count_answers(quest_id):
-    answer_data = []
-    connection.read_data_from_csv("templates/answer.csv", answer_data)
-    return any(answer['question_id'] == quest_id for answer in answer_data)
-
-
-def increase_view_number(quest_id):
-    questions = []
-    connection.read_data_from_csv("templates/question.csv", questions)
-    for question in questions:
-        if quest_id == question['id']:
-            question['view_number'] = int(question['view_number'])
-            question['view_number'] += 1
-    connection.write_data_to_csv("templates/question.csv", QUESTION_FIELDS, questions)
+    any_answer = list_answers()
+    return any(answer['question_id'] == int(quest_id) for answer in any_answer)
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from data_manager import sort_data_by_value, convert_unix_time_to_date, add_question, add_answer, edit_question, \
-    delete_question, delete_answer, count_answers, increase_view_number
+import data_manager
 from util import define_table_headers, get_latest_question_id
 
 app = Flask(__name__)
@@ -13,28 +12,38 @@ def route_index():
 
 @app.route('/list')
 def route_list():
-    questions_list = sort_data_by_value('submission_time', 'question')
-    return render_template('list.html', questions_list=questions_list)
+    all_question = data_manager.list_all_question()
+    return render_template('list.html', all_question=all_question)
 
 
 @app.route('/question/<quest_id>')
 def route_question(quest_id=None):
-    increase_view_number(quest_id)
-    questions = convert_unix_time_to_date('question')
-    sort_data_by_value('submission_time', 'answer')
-    answers = convert_unix_time_to_date('answer')
+    all_answer = data_manager.list_answers()
     table_headers = define_table_headers()
-    any_answer = count_answers(quest_id)
+    questions = data_manager.list_all_question()
+    is_answer = data_manager.count_answers(quest_id)
+    return render_template('question.html', questions=questions, quest_id=int(quest_id), question_headers=table_headers[0], all_answer= all_answer, answer_headers=table_headers[1], is_answer=is_answer)
+
+
+'''
+@app.route('/question/<quest_id>')
+def route_question(quest_id=None):
+    data_manager.increase_view_number(quest_id)
+    questions = data_manager.convert_unix_time_to_date('question')
+    data_manager.sort_data_by_value('submission_time', 'answer')
+    answers = data_manager.convert_unix_time_to_date('answer')
+    table_headers = define_table_headers()
+    any_answer = data_manager.count_answers(quest_id)
     return render_template('question.html', q_fields=table_headers[0], a_fields=table_headers[1],
                            questions=questions, answers=answers, quest_id=quest_id, any_answer=any_answer)
 
 
 @app.route('/question/<quest_id>/new-answer', methods=["GET", "POST"])
 def post_answer(quest_id=None):
-    questions = convert_unix_time_to_date('question')
+    questions = data_manager.convert_unix_time_to_date('question')
     table_headers = define_table_headers()
     if request.method == "POST":
-        add_answer(quest_id, request.form["message"])
+        data_manager.add_answer(quest_id, request.form["message"])
 
         return redirect("/question/" + quest_id)
 
@@ -44,7 +53,7 @@ def post_answer(quest_id=None):
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_ask_question(quest_id=None):
     if request.method == 'POST':
-        add_question(request.form['title'], request.form['message'])
+        data_manager.add_question(request.form['title'], request.form['message'])
         return redirect('/question/' + get_latest_question_id())
     return render_template('add-question.html', quest_id=quest_id)
 
@@ -53,23 +62,23 @@ def route_ask_question(quest_id=None):
 def route_edit_question(quest_id=None):
     if request.method == 'GET':
         update = True
-        questions_list = sort_data_by_value('submission_time', 'question')
+        questions_list = data_manager.sort_data_by_value('submission_time', 'question')
     else:
-        edit_question(request.form['title'], request.form['message'], quest_id)
+        data_manager.edit_question(request.form['title'], request.form['message'], quest_id)
         return redirect('/question/' + quest_id)
     return render_template('add-question.html', quest_id=quest_id, questions_list=questions_list, update=update)
 
 
 @app.route('/question/<quest_id>/delete', methods=['POST'])
 def route_delete_question(quest_id=None):
-    delete_question(quest_id)
+    data_manager.delete_question(quest_id)
     return redirect('/list')
 
 
 @app.route("/answer/<answer_id>/delete", methods=["POST"])
 def route_delete_answer(answer_id=None):
-    question_id = delete_answer(answer_id)
-    return redirect('question/' + question_id)
+    question_id = data_manager.delete_answer(answer_id)
+    return redirect('question/' + question_id)'''
 
 
 if __name__ == "__main__":
